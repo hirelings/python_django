f44aff3a3a3328b0a0a4e84bddd26a4af99418c3
hirelings <hirelings@outlook.com>
2018-08-14 17:22:26 +0800

    $ git push -u origin master
    Signed-off-by: hirelings <hirelings@outlook.com>

---
+++ sign/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+     return render(request,"index.html")
+#登录动作
+def login_action(request):
+    if request.method =='POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        if username =='admin' and password =='admin111':
+            response = HttpResponseRedirect('/event_manage/')
+            # response.set_cookie('user',username,3600) #添加浏览器cookie
+            request.session['user']=username #将session信息记录到浏览器
+            return response
+        else:
+            return render(request,'index.html',{'error':'username or password error!'})
+    else:
+        return render(request, 'index.html', {'error': 'username or password error!'})
+#发布会管理
+def event_manage(request):
+    # username = request.COOKIES.get('user','') #读取浏览器cookie
+    username = request.session.get('user','')   #读取浏览器session
+    return render(request,"event_manage.html",{"user":username})
